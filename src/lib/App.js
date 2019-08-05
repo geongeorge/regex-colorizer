9d77706eb70bc4b5722cd41c3aecabce1e6bc266
Geon George <geongeorge98@gmail.com>
2019-08-05 06:45:04 +0530

    removed using strict

--- src/lib/App.js
+++
@@ -1,9 +0,0 @@
-import './css/App.css';
-
-class App {
-  constructor(){
-    console.log("Lib constructor called");
-  }
-}
-
-export default App;
--- src/lib/regex.js
+++ src/lib/regex.js
@@ -4,7 +4,7 @@
  */
 
 var RegexColorizer = (function () {
-    "use strict";
+    // "use strict";
 
 /*--------------------------------------
  *  Private variables
